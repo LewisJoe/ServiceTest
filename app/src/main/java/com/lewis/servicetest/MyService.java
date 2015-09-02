77d8f196abd0fd5128f67cf1960938321e7d279b
LewisJoe <zhouy0131@gmail.com>
2015-09-02 17:48:12 +0800

    android service practice

---
+++ app/src/main/java/com/lewis/servicetest/MyService.java
@@ -0,0 +1,70 @@
+package com.lewis.servicetest;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+
+/**
+ * 自定义一个服务
+ * Created by Administrator on 15-9-2.
+ */
+public class MyService extends Service {
+    private final static String TAG = "MyService";
+    //创建一个专门的Binder对象来对下载功能进行管理
+    private DownloadBinder mBinder = new DownloadBinder();
+
+    public class DownloadBinder extends Binder{
+
+        public void startDownload(){
+            Log.d(TAG,"startDownload executed");
+        }
+
+        public int getProgress(){
+            Log.d(TAG,"getProgress executed");
+            return 0;
+        }
+    }
+
+    /**
+     * 活动与服务之间的联系
+     * @param intent
+     * @return
+     */
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    /**
+     * 创建服务的时候调用
+     */
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG,"onCreate executed");
+    }
+
+    /**
+     * 每次服务启动的时候调用
+     * @param intent
+     * @param flags
+     * @param startId
+     * @return
+     */
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG,"onStartCommand executed");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    /**
+     * 服务销毁的时候调用
+     */
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"onDestroy executed");
+    }
+}
